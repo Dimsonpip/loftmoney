0c78d2392f5cdffbf0a2333911b7855b1f64a631
Dimsonpip <goodwin0210@protonmail.com>
2020-04-02 01:27:35 +0300

    HW4 without additional

--- app/src/main/java/com/dimsonpip/loftmoney/screens/main/MainActivity.java
+++ app/src/main/java/com/dimsonpip/loftmoney/screens/main/MainActivity.java
@@ -1,22 +1,16 @@
 package com.dimsonpip.loftmoney.screens.main;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.dimsonpip.loftmoney.R;
+import com.dimsonpip.loftmoney.screens.main.adapter.BudgetPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,9 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TabLayout tabLayout = findViewById(R.id.tabs_main);
-
         ViewPager viewPager = findViewById(R.id.viewpager);
-
         viewPager.setAdapter(new BudgetPagerAdapter(getSupportFragmentManager(),
                 FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT));
 
@@ -39,22 +31,4 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(2).setText(R.string.budget);
     }
 
-    static class BudgetPagerAdapter extends FragmentPagerAdapter {
-
-
-        public BudgetPagerAdapter(@NonNull FragmentManager fm, int behavior) {
-            super(fm, behavior);
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            return new BudgetFragment();
-        }
-
-        @Override
-        public int getCount() {
-            return 3;
-        }
-    }
 }
